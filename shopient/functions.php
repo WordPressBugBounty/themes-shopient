f48df3537f1e352f1616924bac8811d6f8b212b2
Sean Murphy <sean@iamseanmurphy.com>
2025-06-27 10:19:33 +0000

    20.1

--- shopient/functions.php
+++ shopient/functions.php
@@ -2,7 +2,7 @@
 /**
  * Define Theme Version
  */
-define( 'SHOPIENT_THEME_VERSION', '20.0' );
+define( 'SHOPIENT_THEME_VERSION', '20.1' );
 
 function shopient_css() {
 	$parent_style = 'storely-parent-style';
